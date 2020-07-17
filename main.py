f351a3b641277acc253f8a94b635a9667e48a0f5
PinkaProgrusition <slkf72@yandex.ru>
2020-07-17 15:48:17 +0300

    Added animation of the game 1

---
+++ main.py
@@ -0,0 +1,20 @@
+from pygame import *
+import sys
+
+init()
+
+screen = display.set_mode((1890, 1417))
+
+display.set_caption("Turing's jorney")
+
+background = image.load('background.jpg')
+
+for i in range(1, 17):
+    background = image.load('./продолжить ряд/'+str(i)+'.png')
+    screen.blit(background, (0, 0))
+    display.flip()
+
+while 1:
+    for i in event.get(): # Перебор в списке событий
+        if i.type == QUIT: # Обрабатываем событие шечка по крестику закрытия окна
+            sys.exit()
