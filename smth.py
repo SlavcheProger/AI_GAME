a74fb3646f8436f8a5c856e687661d5cdc349fec
Lin <galinchernetsova@gmail.com>
2020-07-18 17:43:48 +0300

    all images

--- smth.py
+++ smth.py
@@ -3,25 +3,28 @@ import sys
 
 init()
 
-screen = display.set_mode((640, 480))
+screen = display.set_mode((1890, 1417))
 
 
-display.set_caption('non')
+display.set_caption('none')
 
 
-background = image.load('background.jpg')
+background = image.load('./продолжить ряд/1.png')
 
 screen.blit(background, (0, 0))
-
+for i in range(1, 17):
+    background = image.load('./продолжить ряд/' + str(i) + '.png')
+    screen.blit(background, (0, 0))
+    display.flip()
 
 
 
 while 1:
-
+    for i in event.get():
         if i.type == QUIT:
 
             sys.exit()
 
 
-   
+
 
