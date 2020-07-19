d196fb1bef92c7092a9967390cee76b9de245e29
PinkaProgrusition <slkf72@yandex.ru>
2020-07-19 17:11:21 +0300

    1st game is almost ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import *
 import sys
+from textediting import *
 
 init()
 
@@ -15,9 +16,26 @@ for i in range(1, 17):
     screen.blit(background, (0, 0))
     display.flip()
 
+text = EditText(screen, background, 600, 650)
+print("!!!!",text)
+while text != '1':
+    f = font.SysFont(None, 48)
+    img = f.render('No!!!!', True, (255, 0, 0))
+    screen.blit(img, (0, 0))
+    display.update()
+    text = EditText(screen, background, 600, 650)
+
+print("WIN!")
+
+f = font.SysFont(None, 48)
+img = f.render('WIN!!!!', True, (255,0,0))
+screen.blit(img, (0,0))
+display.update()
+
+running = True
 while running:
     for e in event.get():
         if e.type == QUIT:
             running = False
 
-pygame.quit()
+quit()
